eac71de36bc3f16f0d7d3c0b62f22b319fa3154e
piumi274 <bhagyapiumi274@gmail.com>
2025-06-23 11:43:49 +0530

    MadLib Generator

---
+++ MadLib generator.py
@@ -0,0 +1,10 @@
+print("My name is ________. I am ________ years old and currently live in ________. I work as a ________, and my hobbies include ________. In the future, I hope to ________.")
+
+name = input("Enter name: ")
+age = input("Enter Age: ")
+city = input("Enter city: ")
+job = input("Enter Job: ")
+hobby = input("Enter hobby:")
+future = input("Enter future goal: ")
+
+print(f"My name is {name}. I am {age} years old and currently live in {city}. I work as a {job}, and my hobbies include {hobby} . In the future, I hope to {future}.")
